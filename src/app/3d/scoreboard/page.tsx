504e1d92ea9e5118232553c10c7e6e2f2acb4e5c
Andrei Pinto <andreiqp@outlook.com>
2026-08-13 12:18:57 -0300

    alterando 3D para estética 2026

--- src/app/3d/scoreboard/page.tsx
+++ src/app/3d/scoreboard/page.tsx
@@ -41,7 +41,10 @@ const MainScore: React.FC = () => {
         p: 4,
         bgcolor: '#000000',
         minHeight: '100vh',
-        backgroundImage: 'url("/Fundo.png")'
+        backgroundImage: 'url("/Fundo.png")',
+        backgroundSize: '100% 100%',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
       }}
     >
       <Box
